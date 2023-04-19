19de0ca7a06f467ac744639ee0dc3ba7a1ecd2d2
Piotr Milecki <piotr.milecki96@gmail.com>
2023-04-19 20:25:34 +0200

    fix phpstan errors

--- backend/tests/Feature/ExampleTest.php
+++ backend/tests/Feature/ExampleTest.php
@@ -1,7 +1,9 @@
 <?php
 
+use function Pest\Laravel\get;
+
 it('returns a successful response', function () {
-    $response = $this->get('/');
+    $response = get('/');
 
     $response->assertStatus(200);
 });
