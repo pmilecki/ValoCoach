ce634971988670e433264d1882af90a905f6b5be
Piotr Milecki <piotr.milecki96@gmail.com>
2023-04-19 19:41:25 +0200

    frontend init

---
+++ frontend/.lintstagedrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    ".js": [
+      "eslint --fix --max-warnings=0 --report-unused-disable-directives"
+    ],
+    ".ts": [
+      "eslint --fix --max-warnings=0 --report-unused-disable-directives"
+    ],
+    ".vue": [
+      "eslint --fix --max-warnings=0 --report-unused-disable-directives"
+    ]
+}
